904e4f5048f439d498610c95f21e949ea59b9ac8
Peter Brown <phbrown@acm.org>
2021-04-23 02:29:25 +0000

    Added database connection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Load the Express framework
 const express = require('express');
+// Load the sqlite3 module
+const sqlite3 = require('sqlite3').verbose();
 
 // Create the app object
 const app = express();
@@ -20,6 +22,29 @@ app.engine('hbs', handlebars({
 // Invoke the handler for static files
 app.use(express.static('public'));
 
+
+function openDB(filename) {
+    if (!filename) {
+        filename = __dirname + "/SQL/" + "grasshopper-air.sqlite";
+    }
+    return new sqlite3.Database(filename,
+        (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+            console.log('Connected to ' + filename);
+        })
+}
+
+function closeDB(database) {
+    database.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Closed the database connection.');
+    });
+}
+
 // Give the app a handler for a GET request to
 //   the root of the document tree.  The app
 //   handler takes the request and response
@@ -29,14 +54,26 @@ app.use(express.static('public'));
 //});
 
 app.get('/', (req, res) => {
+    let db = openDB();
     if (!req.query.form_submission) {
         //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
         res.render('main');
     }
     else if (req.query.to_do === 'query') {
-        res.render('query', {
-             first_name:req.query.first_name,
-             last_name:req.query.last_name
+        sql = "select * from Passengers where FirstName=$first and LastName=$last;";
+        db.get(sql, { 
+                $first: req.query.first_name, 
+                $last: req.query.last_name
+            }, (err, row) => {
+            if (err) {
+                throw err;
+            }
+            console.log(row);
+            res.render('query', {
+                first_name:req.query.first_name,
+                last_name:req.query.last_name,
+                person: row
+            });
         });
     }
     else if (req.query.to_do === 'add') {
@@ -46,8 +83,16 @@ app.get('/', (req, res) => {
         });
     }
     else { // List users
-        res.render('listusers');
+        let sql = "select * from passengers;";
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            //console.log(rows);
+            res.render('listusers', {passengers: rows});
+        })
     }
+    closeDB(db);
 });
 
 // app.get('/', function (req, res) {
@@ -73,3 +118,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('server started');
 });
+
